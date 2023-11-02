b92aadd27b7b485c6b1c616f49801e3a98fccfdf
Talha Demirbas <talhaademirbas@gmail.com>
2023-11-02 11:07:23 +0300

    minor changes on constants file

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static Color white1 = const Color(0xffffffff);
-  static Color white2 = const Color(0xfff6f8fa);
-  static Color white3 = const Color(0xffeaeef2);
+  static const Color white1 = Color(0xffffffff);
+  static const Color white2 = Color(0xfff6f8fa);
+  static const Color white3 = Color(0xffeaeef2);
 
-  static Color grey1 = const Color(0xff57606a);
-  static Color grey2 = const Color(0xff24292f);
-  static Color shadowColor = Colors.grey.shade400;
-  static Color yellowAccent = const Color(0xfffbc100);
-  static Color blue = const Color(0xff0969da);
+  static const Color grey1 = Color(0xff57606a);
+  static const Color grey2 = Color(0xff24292f);
+  static const Color shadowColor = Color.fromARGB(255, 189, 189, 189);
+
+  static const Color yellowAccent = Color(0xfffbc100);
+  static const Color blue = Color(0xff0969da);
 }
 
 class MySizes {
-  static double kDefaultFont = 12;
-  static double kSmallFont = 11;
+  static const double kDefaultFont = 12.0;
+  static const double kSmallFont = 11.0;
 
-  static double kDefaultPadding = 10;
-  static double kSmallPadding = 8;
+  static const double kDefaultPadding = 10.0;
+  static const double kSmallPadding = 8.0;
 
-  static double kDefaultIcon = 18;
-  static double kSmallIcon = 14;
+  static const double kDefaultIcon = 18.0;
+  static const double kSmallIcon = 14.0;
 
-  static double kCircle = 26;
-  static double kBoldCircle = 28;
+  static const double kCircle = 26.0;
+  static const double kBoldCircle = 28.0;
 }
